0952f68f6c88042b0c1bea1e154469e4ea1aea0a
0xchase <chasekanipe@gmail.com>
2022-11-17 09:40:50 -0500

    More rorganization

--- flutter/lib/instrument.dart
+++ flutter/lib/instrument.dart
@@ -35,44 +35,37 @@ class _InstrumentView extends State<InstrumentView> {
   @override
   Widget build(BuildContext context) {
     return ValueListenableBuilder<bool>(
-      valueListenable: widget.tree.editing,
-      builder: (context, editing, child) {
-        if (editing) {
-          return Container(
-            color: Colors.black,
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                WidgetMenu(),
-                Expanded(
-                  child: Container(
-                    alignment: Alignment.center,
-                    color: Colors.black,
-                    child: display,
+        valueListenable: widget.tree.editing,
+        builder: (context, editing, child) {
+          if (editing) {
+            return Container(
+                color: Colors.black,
+                child: Row(mainAxisSize: MainAxisSize.max, children: [
+                  WidgetMenu(),
+                  Expanded(
+                    child: Container(
+                      alignment: Alignment.center,
+                      color: Colors.black,
+                      child: display,
+                    ),
                   ),
-                ),
-                Column(
-                  children: [
-                    Expanded(
-                      child: WidgetTreeMenu(widget.tree, widget.host)
-                    ),
-                    Expanded(
-                      child: WidgetEditorMenu(widget.tree),
-                    ),
-                  ],
-                )
-              ]
-            )
-          );
-        } else {
-          return Container(
-            alignment: Alignment.center,
-            color: Colors.black,
-            child: display,
-          );
-        }
-      }
-    );
+                  Column(
+                    children: [
+                      Expanded(child: WidgetTreeMenu(widget.tree, widget.host)),
+                      Expanded(
+                        child: WidgetEditorMenu(widget.tree),
+                      ),
+                    ],
+                  )
+                ]));
+          } else {
+            return Container(
+              alignment: Alignment.center,
+              color: Colors.black,
+              child: display,
+            );
+          }
+        });
   }
 }
 
@@ -94,35 +87,29 @@ class _UIDisplay extends State<UIDisplay> {
   Widget build(BuildContext context) {
     widget.host.globals.rootWidget ??= RootWidget(widget.host, widget.tree);
 
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return Scrollbar(
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scrollbar(
           controller: vertical,
           thumbVisibility: true,
           trackVisibility: true,
           child: Scrollbar(
-            controller: horizontal,
-            thumbVisibility: true,
-            trackVisibility: true,
-            notificationPredicate: (notif) => notif.depth == 1,
-            child: SingleChildScrollView(
-              controller: vertical,
+              controller: horizontal,
+              thumbVisibility: true,
+              trackVisibility: true,
+              notificationPredicate: (notif) => notif.depth == 1,
               child: SingleChildScrollView(
-                controller: horizontal,
-                scrollDirection: Axis.horizontal,
-                child: Container(
-                  width: 1000,
-                  height: 1000,
-                  alignment: Alignment.center,
-                  color: Colors.grey,
-                  child: widget.host.globals.rootWidget,
-                )
-              )
-            )
-          )
-        );
-      }
-    );
+                  controller: vertical,
+                  child: SingleChildScrollView(
+                      controller: horizontal,
+                      scrollDirection: Axis.horizontal,
+                      child: Container(
+                        width: 1000,
+                        height: 1000,
+                        alignment: Alignment.center,
+                        color: Colors.grey,
+                        child: widget.host.globals.rootWidget,
+                      )))));
+    });
   }
 }
 
@@ -142,28 +129,27 @@ class _WidgetTreeMenu extends State<WidgetTreeMenu> {
   @override
   Widget build(BuildContext context) {
     return ValueListenableBuilder(
-      valueListenable: widget.tree.editing,
-      builder: (context, value, child) {
-        return Container(
-          width: 300,
-          color: const Color.fromRGBO(30, 30, 30, 1.0),
-          child: Column(
-            children: [
-              EditorTitle("Widget Tree"),
-              Expanded(
-                child: SingleChildScrollView(
+        valueListenable: widget.tree.editing,
+        builder: (context, value, child) {
+          return Container(
+            width: 300,
+            color: const Color.fromRGBO(30, 30, 30, 1.0),
+            child: Column(
+              children: [
+                EditorTitle("Widget Tree"),
+                Expanded(
+                    child: SingleChildScrollView(
                   controller: controller,
-                  child: widget.host.globals.rootWidget != null ? WidgetTreeElement(
-                    widget: widget.host.globals.rootWidget!,
-                    tree: widget.tree
-                  ) : Container(),
-                )
-              )
-            ],
-          ),
-        );
-      }
-    );
+                  child: widget.host.globals.rootWidget != null
+                      ? WidgetTreeElement(
+                          widget: widget.host.globals.rootWidget!,
+                          tree: widget.tree)
+                      : Container(),
+                ))
+              ],
+            ),
+          );
+        });
   }
 }
 
@@ -184,41 +170,35 @@ class WidgetTreeElement extends StatelessWidget {
     }
 
     return ExpansionTile(
-      title: Row(
-        children: [
-          Text(
-            widget.getName(),
-            style: const TextStyle(
-              color: Colors.white,
-              fontSize: 14
-            ),
-          ),
-          widget.getName() == "Empty" || widget.getName() == "Root" ? Container() : 
-          IconButton(
-            icon: const Icon(
-              Icons.delete,
-              color: Colors.white,
-              size: 16,
-            ),
-            onPressed: () {
-              tree.delete(widget);
-            },
-          )
-        ]
-      ),
+      title: Row(children: [
+        Text(
+          widget.getName(),
+          style: const TextStyle(color: Colors.white, fontSize: 14),
+        ),
+        widget.getName() == "Empty" || widget.getName() == "Root"
+            ? Container()
+            : IconButton(
+                icon: const Icon(
+                  Icons.delete,
+                  color: Colors.white,
+                  size: 16,
+                ),
+                onPressed: () {
+                  tree.delete(widget);
+                },
+              )
+      ]),
       initiallyExpanded: true,
       children: [
         Padding(
-          padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
-          child: Column(
-            children: children.map((child) {
+            padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+            child: Column(
+                children: children.map((child) {
               return WidgetTreeElement(
                 widget: child,
                 tree: tree,
               );
-            }).toList()
-          )
-        )
+            }).toList()))
       ],
       textColor: Colors.white,
       collapsedTextColor: Colors.grey,
@@ -243,22 +223,18 @@ class _WidgetEditorMenu extends State<WidgetEditorMenu> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 300,
-      color: const Color.fromRGBO(30, 30, 30, 1.0),
-      child: ValueListenableBuilder<Widget Function(BuildContext)?>(
-        valueListenable: widget.tree.editorBuilder,
-        builder: (context, buildEditor, child) {
-          if (buildEditor == null) {
-            return Container();
-          } else {
-            return SingleChildScrollView(
-              controller: controller,
-              child: buildEditor(context)
-            );
-          }
-        }
-      )
-    );
+        width: 300,
+        color: const Color.fromRGBO(30, 30, 30, 1.0),
+        child: ValueListenableBuilder<Widget Function(BuildContext)?>(
+            valueListenable: widget.tree.editorBuilder,
+            builder: (context, buildEditor, child) {
+              if (buildEditor == null) {
+                return Container();
+              } else {
+                return SingleChildScrollView(
+                    controller: controller, child: buildEditor(context));
+              }
+            }));
   }
 }
 
@@ -273,65 +249,47 @@ class _WidgetMenu extends State<WidgetMenu> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 300,
-      color: const Color.fromRGBO(30, 30, 30, 1.0),
-      padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-      child: Column(
-        children: [
+        width: 300,
+        color: const Color.fromRGBO(30, 30, 30, 1.0),
+        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+        child: Column(children: [
           EditorTitle("Widgets"),
           Expanded(
-            child: SingleChildScrollView(
-              controller: controller,
-              child: Column(
-                children: [
-                  WidgetMenuSection(
-                    title: "Layout",
-                    children: [
-                        WidgetMenuElement("Stack", Icons.stacked_bar_chart),
-                        WidgetMenuElement("Row", Icons.table_rows),
-                        WidgetMenuElement("Column", Icons.table_rows),
-                        WidgetMenuElement("Grid", Icons.grid_4x4),
-                    ]
-                  ),
-                  WidgetMenuSection(
-                    title: "Decoration",
-                    children: [
-                        WidgetMenuElement("Box", Icons.add_box),
-                        WidgetMenuElement("Text", Icons.text_fields),
-                        WidgetMenuElement("Image", Icons.image),
-                        WidgetMenuElement("Icon", Icons.picture_as_pdf),
-                        // WidgetMenuElement("Polygon", Icons.picture_as_pdf),
-                        // WidgetMenuElement("Square", Icons.picture_as_pdf),
-                        // WidgetMenuElement("Line", Icons.picture_as_pdf),
-                    ]
-                  ),
-                  WidgetMenuSection(
-                    title: "Interactive",
-                    children: [
-                        WidgetMenuElement("Dropdown", Icons.list),
-                        WidgetMenuElement("Knob", Icons.king_bed_outlined),
-                        WidgetMenuElement("Slider", Icons.slideshow_rounded),
-                        WidgetMenuElement("Button", Icons.radio_button_checked),
-                        WidgetMenuElement("Envelope", Icons.graphic_eq),
-                        WidgetMenuElement("Mod Source", Icons.graphic_eq),
-                    ]
-                  ),
-                  WidgetMenuSection(
-                    title: "Metering",
-                    children: [
-                        WidgetMenuElement("RMS", Icons.king_bed_outlined),
-                        WidgetMenuElement("Spectrum", Icons.slideshow_rounded),
-                        WidgetMenuElement("Oscilliscope", Icons.radio_button_checked),
-                        WidgetMenuElement("Meter", Icons.padding),
-                    ]
-                  )
-                ]
-              )
-            )
-          )
-        ]
-      )
-    );
+              child: SingleChildScrollView(
+                  controller: controller,
+                  child: Column(children: [
+                    WidgetMenuSection(title: "Layout", children: [
+                      WidgetMenuElement("Stack", Icons.stacked_bar_chart),
+                      WidgetMenuElement("Row", Icons.table_rows),
+                      WidgetMenuElement("Column", Icons.table_rows),
+                      WidgetMenuElement("Grid", Icons.grid_4x4),
+                    ]),
+                    WidgetMenuSection(title: "Decoration", children: [
+                      WidgetMenuElement("Box", Icons.add_box),
+                      WidgetMenuElement("Text", Icons.text_fields),
+                      WidgetMenuElement("Image", Icons.image),
+                      WidgetMenuElement("Icon", Icons.picture_as_pdf),
+                      // WidgetMenuElement("Polygon", Icons.picture_as_pdf),
+                      // WidgetMenuElement("Square", Icons.picture_as_pdf),
+                      // WidgetMenuElement("Line", Icons.picture_as_pdf),
+                    ]),
+                    WidgetMenuSection(title: "Interactive", children: [
+                      WidgetMenuElement("Dropdown", Icons.list),
+                      WidgetMenuElement("Knob", Icons.king_bed_outlined),
+                      WidgetMenuElement("Slider", Icons.slideshow_rounded),
+                      WidgetMenuElement("Button", Icons.radio_button_checked),
+                      WidgetMenuElement("Envelope", Icons.graphic_eq),
+                      WidgetMenuElement("Mod Source", Icons.graphic_eq),
+                    ]),
+                    WidgetMenuSection(title: "Metering", children: [
+                      WidgetMenuElement("RMS", Icons.king_bed_outlined),
+                      WidgetMenuElement("Spectrum", Icons.slideshow_rounded),
+                      WidgetMenuElement(
+                          "Oscilliscope", Icons.radio_button_checked),
+                      WidgetMenuElement("Meter", Icons.padding),
+                    ])
+                  ])))
+        ]));
   }
 }
 
@@ -340,20 +298,19 @@ class WidgetMenuSection extends StatelessWidget {
 
   String title;
   List<Widget> children;
-  
+
   @override
   Widget build(BuildContext context) {
     return Section(
-      title: title,
-      child: GridView.count(
-        mainAxisSpacing: 10,
-        crossAxisSpacing: 10,
-        crossAxisCount: 3,
-        shrinkWrap: true,
-        physics: const NeverScrollableScrollPhysics(),
-        children: children,
-      )
-    );
+        title: title,
+        child: GridView.count(
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+          crossAxisCount: 3,
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          children: children,
+        ));
   }
 }
 
@@ -373,59 +330,49 @@ class _WidgetMenuElement extends State<WidgetMenuElement> {
   @override
   Widget build(BuildContext context) {
     var icon = Container(
-      width: 80,
-      height: 80,
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(5.0),
-        border: Border.all(
-          color: hovering ? Colors.blue : const Color.fromRGBO(60, 60, 60, 1.0),
-          width: 2.0
-        )
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
+        width: 80,
+        height: 80,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5.0),
+            border: Border.all(
+                color: hovering
+                    ? Colors.blue
+                    : const Color.fromRGBO(60, 60, 60, 1.0),
+                width: 2.0)),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
           Icon(
             widget.iconData,
             size: 32,
             color: hovering ? Colors.blue : Colors.grey,
           ),
           Container(
-            height: 20,
-            alignment: Alignment.bottomCenter,
-            child: Text(
-              widget.text,
-              style: TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.w400,
-                fontFamily: DefaultTextStyle.of(context).style.fontFamily,
-                decoration: TextDecoration.none,
-                color: hovering ? Colors.blue : Colors.grey
-              )
-            )
-          )
-        ]
-      )
-    );
+              height: 20,
+              alignment: Alignment.bottomCenter,
+              child: Text(widget.text,
+                  style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                      fontFamily: DefaultTextStyle.of(context).style.fontFamily,
+                      decoration: TextDecoration.none,
+                      color: hovering ? Colors.blue : Colors.grey)))
+        ]));
 
     return MouseRegion(
-      onEnter: (e) => setState(() {
-        hovering = true;
-      }),
-      onExit: (e) => setState(() {
-        hovering = false;
-      }),
-      child: GestureDetector(
-        onTap: () {
-          print("Clicked " + widget.text);
-        },
-        child: Draggable<String>(
-          feedback: icon,
-          child: icon,
-          data: widget.text,
-        )
-      )
-    );
+        onEnter: (e) => setState(() {
+              hovering = true;
+            }),
+        onExit: (e) => setState(() {
+              hovering = false;
+            }),
+        child: GestureDetector(
+            onTap: () {
+              print("Clicked " + widget.text);
+            },
+            child: Draggable<String>(
+              feedback: icon,
+              child: icon,
+              data: widget.text,
+            )));
   }
 }
