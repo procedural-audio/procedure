d7492437dac27bb36124587ead6790134f2bed79
0xchase <chasekanipe@gmail.com>
2022-11-30 17:35:32 -0500

    updates

--- host/macos/standalone/main.mm
+++ host/macos/standalone/main.mm
@@ -1,24 +1,19 @@
 // #include "MainComponent.h"
 // #include "PluginEditor.h"
 
-// #import "FlutterViewController.h"
-// #import "FlutterChannels.h"
 #include "NodusProcessor.h"
 #include "NodusEditor.h"
 #include "JuceHeader.h"
 
-//==============================================================================
 class GuiAppApplication  : public juce::JUCEApplication
 {
 public:
-    //==============================================================================
     GuiAppApplication() {}
 
     const juce::String getApplicationName() override       { return "Application Name"; }
     const juce::String getApplicationVersion() override    { return "Version String Here"; }
     bool moreThanOneInstanceAllowed() override             { return true; }
 
-    //==============================================================================
     void initialise (const juce::String& commandLine) override
     {
         juce::ignoreUnused (commandLine);
@@ -30,7 +25,6 @@ public:
         mainWindow = nullptr;
     }
 
-    //==============================================================================
     void systemRequestedQuit() override
     {
         quit();
@@ -40,8 +34,6 @@ public:
     {
         juce::ignoreUnused (commandLine);
     }
-
-    //==============================================================================
 
     class MainWindow    : public juce::DocumentWindow
     {
@@ -53,14 +45,12 @@ public:
                               DocumentWindow::allButtons)
         {
             setUsingNativeTitleBar (true);
-            // setContentOwned (new Flutter_juceAudioProcessorEditor(), true);
             setContentOwned(new NodusEditor(processor), true);
 
            #if JUCE_IOS || JUCE_ANDROID
             setFullScreen (true);
            #else
             setResizable (true, true);
-            // centreWithSize (getWidth(), getHeight());
             centreWithSize(1200, 900);
            #endif
 
@@ -69,9 +59,6 @@ public:
 
         void closeButtonPressed() override
         {
-            // This is called when the user tries to close this window. Here, we'll just
-            // ask the app to quit when this happens, but you can change this to do
-            // whatever you need.
             JUCEApplication::getInstance()->systemRequestedQuit();
         }
 
@@ -84,6 +71,4 @@ private:
     std::unique_ptr<MainWindow> mainWindow;
 };
 
-//==============================================================================
-// This macro generates the main() routine that launches the app.
 START_JUCE_APPLICATION (GuiAppApplication)
