835f3606df085f2697d2b8ddf9b218f8e29d09fa
0xchase <chasekanipe@gmail.com>
2023-06-10 17:13:12 -0400

    update

--- flutter/lib/views/newTopBar.dart
+++ flutter/lib/views/newTopBar.dart
@@ -1,12 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:bitsdojo_window/bitsdojo_window.dart';
-
-import '../patch.dart';
-import '../main.dart';
-import '../projects.dart';
-
-import 'browser.dart';
-import 'presets.dart';
 
 class NewTopBar extends StatefulWidget {
   NewTopBar({
