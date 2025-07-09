74fcbe93603b94deda23f37ab0964da397620cf5
0xchase <chasekanipe@gmail.com>
2025-07-08 21:17:28 -0400

    fixed build on linux

--- flutter/rust/src/frb_generated.rs
+++ flutter/rust/src/frb_generated.rs
@@ -34,7 +34,7 @@ use flutter_rust_bridge::for_generated::byteorder::{NativeEndian, ReadBytesExt, 
 use flutter_rust_bridge::for_generated::{transform_result_dco, Lifetimeable, Lockable};
 use flutter_rust_bridge::{Handler, IntoIntoDart};
 
-use std::sync::{Mutex};
+use std::sync::Mutex;
 use cmajor::performer::Performer;
 
 // Section: boilerplate
