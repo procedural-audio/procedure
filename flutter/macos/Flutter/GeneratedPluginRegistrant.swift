1eafd633c813c946abfac4229fb9c77c528e5a56
Chase Kanipe <chasekanipe@gmail.com>
2025-10-25 13:28:08 -0400

    removed webview for now

--- flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -15,7 +15,6 @@ import firebase_core
 import google_sign_in_ios
 import path_provider_foundation
 import shared_preferences_foundation
-import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
@@ -28,5 +27,4 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
